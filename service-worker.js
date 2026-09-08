7cb2c33f18ea0ebac8d33c1a82d712882230952d
heitoribeiro <heitortbt@gmail.com>
2026-09-07 22:27:19 -0300

    build: renova cache PWA para menu 8235

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='betel-radar-v0.8.2-github-8234';
+const CACHE='betel-radar-v0.8.2-github-8235';
 const ASSETS=[
   './','./index.html','./manifest.webmanifest','./icon.svg',
   './chunk00.txt','./chunk01.txt','./chunk02.txt','./chunk03.txt',
